f0374220e557c2f8880820f2d76eafe6ce8986ae
alexismarroquin7 <alexismarroquin754@gmail.com>
2021-04-15 15:20:07 -0700

    provide seed data to db

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ module.exports = {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './data/cookbook.db'
+      filename: './data/cookbook.db3'
     },
     migrations: {
       directory: './data/migrations'
