a44f8f4c20b6c7024584a6c57f97128113745f23
alexismarroquin7 <alexismarroquin754@gmail.com>
2021-04-15 15:13:16 -0700

    insert seed data

--- data/seeds/02-ingredients.js
+++ data/seeds/02-ingredients.js
@@ -1,13 +1,10 @@
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex('ingredients').insert([
+    { ingredient_name: 'Meatballs' },
+    { ingredient_name: 'Marinara Sauce' },
+    { ingredient_name: 'Fettuccine' },
+    { ingredient_name: 'New York Steak' },
+    { ingredient_name: 'Olive oil' }
+  ]);
 };
--- data/seeds/03-steps.js
+++ data/seeds/03-steps.js
@@ -1,13 +1,10 @@
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex('steps').insert([
+    { step_instructions: 'Put a large saucepan on a medium heat', step_number: 1, recipe_id: 1 },
+    { step_instructions: 'Add 2 cans of Marinara sauce', step_number: 2, recipe_id: 1 },
+    { step_instructions: 'Add 1 box of Fettuccine', step_number: 3, recipe_id: 1 },
+    { step_instructions: 'Pat the steak dry', step_number: 1, recipe_id: 2 },
+    { step_instructions: 'Add 1 tbsp olive oil', step_number: 2, recipe_id: 2 }
+  ]);
 };
