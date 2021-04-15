861c571af9708f8067ed2d0eb07e645b088efee3
alexismarroquin7 <alexismarroquin754@gmail.com>
2021-04-15 16:48:21 -0700

    findById - returns recipe && steps, ! ingreds

--- api/recipies/recipe-model.js
+++ api/recipies/recipe-model.js
@@ -4,9 +4,21 @@ const findAll = () => {
   return db('recipies');
 }
 
-const findById = recipe_id => {
-  return db('recipies as r')
-    .where('r.recipe_id', recipe_id);
+const findById = async recipe_id => {
+  const [ recipe_row ] = await db('recipies as r').where('r.recipe_id', recipe_id);
+  const steps = await db('recipies as r')
+  .leftJoin('steps as s', 'r.recipe_id', 's.recipe_id')
+    .select(
+      'r.recipe_id',
+      's.step_id',
+      's.step_instructions', 
+      's.step_number'
+    )
+    .where('s.recipe_id', recipe_id)
+    .orderBy('s.step_number', 'asc');
+  console.log("recipe_row: ", recipe_row)
+  const result = { ...recipe_row, steps };
+  return result;
 }
 
 module.exports = {
